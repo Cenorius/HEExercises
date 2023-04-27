6d56cd7ce85b09f8d331faca42390b29f152b414
cenorius <dave.ayuso@gmail.com>
2023-04-27 11:01:56 -0400

    change background color and require_once head

--- app/head.php
+++ app/head.php
@@ -15,17 +15,18 @@
     integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==">
   <link href="/public/assets/css/theme.css" rel="stylesheet" />
   <link href="/public/vendors/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../../../public/assets/css/custom-theme.css">
 </head>
 
 <body>
-  <main class="main" id="top">
+  <main class="main main-background" id="top">
     <!-- NAV START -->
     <nav class="navbar navbar-light sticky-top" data-navbar-darken-on-scroll="900">
       <div class="container"><a class="navbar-brand" href=""> 
         <img src="/public/assets/img/gallery/logo.png" alt="..." /></a>
         <div class="navbar-nav ms-auto">
-        <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+  <!--      <div class="dropdown">
+  -<button class="btn btn-secondary dropdown-toggle d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
     <?php
 
       echo '<img src="/public/assets/img/'.$lang.'.png" style="width: 26px; margin-right:5px;" /> '.getLangName($lang).'';
@@ -36,7 +37,7 @@
     <li><a class="dropdown-item" href="#" onclick="setLanguage('en')"><img src="/public/assets/img/en.png" style="width: 26px; margin-right:5px;" />English </a></li>
     <li><a class="dropdown-item" href="#" onclick="setLanguage('tr')"><img src="/public/assets/img/tr.png" style="width: 26px; margin-right:5px;" />Türkçe </a></li>
     <li><a class="dropdown-item" href="#" onclick="setLanguage('fr')"><img src="/public/assets/img/fr.png" style="width: 26px; margin-right:5px;" />Français </a></li>
-  </ul>
+  </ul>-->
 </div>
 
         </div>
