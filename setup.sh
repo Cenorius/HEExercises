b23468185209ad984295c2bd6f113036c9b5e4de
cenorius <dave.ayuso@gmail.com>
2023-05-02 11:00:17 -0400

    add dump, flags and setup.sh

--- setup.sh
+++ setup.sh
@@ -30,9 +30,52 @@ chown -R www-data:www-data /var/www/html
 rm /var/www/html/index.html
 cp -R app/* /var/www/html/
 
-mysql -u root -e "CREATE DATABASE sql_injection;"
+mysql -u root -e "CREATE DATABASE sqli_login"
+mysql -u root -e "CREATE USER'sqli_login'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_login. * TO 'sqli_login'@'localhost';"
+
+mysql -u root -e "CREATE DATABASE sqli_union"
+mysql -u root -e "CREATE USER'sqli_union'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users TO 'sqli_union'@'localhost';"
+mysql -u root -e "GRANT SELECT ON sqli_union.flag TO 'sqli_union'@'localhost';"
+
+mysql -u root -e "CREATE USER'sqli_union_post'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users  TO 'sqli_union_post'@'localhost';"
+mysql -u root -e "GRANT SELECT ON sqli_union.flag2  TO 'sqli_union_post'@'localhost';"
+
+mysql -u root -e "CREATE USER'sqli_cookie'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users  TO 'sqli_cookie'@'localhost';"
+mysql -u root -e "GRANT SELECT ON sqli_union.flagc4  TO 'sqli_cookie'@'localhost';"
+
+mysql -u root -e "CREATE USER'sqli_jason'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users  TO 'sqli_jason'@'localhost';"
+mysql -u root -e "GRANT SELECT ON sqli_union.treasure  TO 'sqli_jason'@'localhost';"
+
+mysql -u root -e "CREATE USER'sqli_or'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users  TO 'sqli_or'@'localhost';"
+mysql -u root -e "GRANT SELECT ON sqli_union.ironthrone  TO 'sqli_or'@'localhost';"
+
+
+mysql -u root -e "CREATE USER'sqli_boundaries'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users  TO 'sqli_boundaries'@'localhost';"
+mysql -u root -e "GRANT SELECT ON sqli_union.limits  TO 'sqli_boundaries'@'localhost';"
+
+mysql -u root -e "CREATE USER'sqli_csrf'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users  TO 'sqli_csrf'@'localhost';"
+mysql -u root -e "GRANT SELECT ON sqli_union.csrfFlag  TO 'sqli_csrf'@'localhost';"
+
+mysql -u root -e "CREATE USER'sqli_os'@'localhost' IDENTIFIED BY 'contrafacil';"
+mysql -u root -e "GRANT SELECT ON sqli_union.users  TO 'sqli_os'@'localhost';"
+mysql -u root -e "GRANT FILE ON sqli_union.users  TO 'sqli_os'@'localhost';"
+
+echo '[mysqld]' >> /etc/mysql/my.cnf
+echo 'secure_file_priv="/var/www/html/carpetaSecreta/"' >> /etc/mysql/my.cnf
+
+
+mysql -u root -e "CREATE DATABASE sql_injection"
+
 mysql -u root sql_injection < /var/www/html/lab/sql-injection/dump.sql
-mysql -u root -e "CREATE USER 'sql_injection'@'localhost' IDENTIFIED BY '';"
+mysql -u root -e "CREATE USER'sql_injection'@'localhost' IDENTIFIED BY 'contrafacil';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'sql_injection'@'localhost';"
 
 
