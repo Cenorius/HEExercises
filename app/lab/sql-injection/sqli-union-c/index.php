36c51065781e0f3785b24d1bc6a096fbea50d83e
cenorius <dave.ayuso@gmail.com>
2023-05-02 07:48:41 -0400

    update sqli web tittles

--- app/lab/sql-injection/sqli-union-c/index.php
+++ app/lab/sql-injection/sqli-union-c/index.php
@@ -20,7 +20,7 @@ if(!isset($_COOKIE['name'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-    <title> <?php echo $strings['kayit'] ?> </title>
+    <title> SQLi cookie</title>
     <style>
       body {
         margin: 0;
--- app/lab/sql-injection/sqli-union-jason/index.php
+++ app/lab/sql-injection/sqli-union-jason/index.php
@@ -18,7 +18,7 @@ if( empty(file_get_contents("php://input"))){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-    <title> <?php echo $strings['kayit'] ?> </title>
+    <title> SQLi Jason</title>
     <style>
       body {
         margin: 0;
